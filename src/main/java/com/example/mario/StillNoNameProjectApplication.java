6b3d3bdd166c7510f89e386c691d10442fe98b6f
Mario Penkov <mario.penkov@car-digital.eu>
2025-12-16 16:48:22 +0200

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/example/mario/StillNoNameProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StillNoNameProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StillNoNameProjectApplication.class, args);
+	}
+
+}
