d39d8cb4bb99334b83a97b94b1ff18ff50133fac
Mario Penkov <mario.penkov@car-digital.eu>
2026-01-12 17:21:12 +0200

    Initial commit

--- src/main/java/com/example/mario/StillNoNameProjectApplication.java
+++ src/main/java/com/example/mario/StillNoNameProjectApplication.java
@@ -1,14 +1,21 @@
 package com.example.mario;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StillNoNameProjectApplication {
 
 	public static void main(String[] args) {
-        System.out.println("Hello World");
 		SpringApplication.run(StillNoNameProjectApplication.class, args);
     }
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
